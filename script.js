9446b94b1f296143993696650c9333b7a1bb37dd
Yayitodwl <73145786+Yayitodwl@users.noreply.github.com>
2023-04-02 22:52:40 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ navLinks.forEach(link => {
     link.classList.add('active');
   });
 });
+
+
+const productos = document.querySelectorAll('.producto');
+
+productos.forEach(producto => {
+  producto.addEventListener('mouseover', () => {
+    const descripcion = producto.querySelector('p');
+    descripcion.style.display = 'block';
+  });
+
+  producto.addEventListener('mouseout', () => {
+    const descripcion = producto.querySelector('p');
+    descripcion.style.display = 'none';
+  });
+});
